574f95d7c56e3d53ad31d078a12f2cbd9cc5b675
diliprajbongshi~ <dr1117255@gmail.com>
2022-08-05 22:21:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Hello from './mycomponent/Hello';
 import Welcome from './mycomponent/Welcome';
-import New from './mycomponent/New';
-import Four from './mycomponent/Four';
-import Five from './mycomponent/Four';
+// import New from './mycomponent/New';
+// import Four from './mycomponent/Four';
+// import Five from './mycomponent/Four';
 
 function App() {
   return (
     <div className="App">
         <Hello name="dilip"  age="30"></Hello>
       
+      <Welcome name="class one" ></Welcome>
       <Welcome name="class one" ></Welcome>
 
    
